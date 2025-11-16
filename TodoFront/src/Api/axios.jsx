11e5b6ebefd0a383a077fdb14ba1955b2b7d4a29
Santiago-Gauna <rossigauna43@gmail.com>
2025-11-16 15:28:19 -0300

    docker compose

--- TodoFront/src/Api/axios.jsx
+++ TodoFront/src/Api/axios.jsx
@@ -1,43 +1,43 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://backend:3000/api/",
 });
 
 const axiosRegister = axios.create({
-  baseURL: "http://localhost:3000/api/login/",
+  baseURL: "http://backend:3000/api/login/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 const axiosLogin = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://backend:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 const axiosPost = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://backend:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 const axiosGet = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://backend:3000/api/",
 });
 
 const axiosPut = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://backend:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 const axiosDelete = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://backend:3000/api/",
   headers: {
     "Content-Type": "application/json",
   },
